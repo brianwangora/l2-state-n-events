dec1ffc4c0b9d65e06a3b4f2f8dd14e1f5552ba6
Brian Wangora <brianwangora@gmail.com>
2022-07-14 14:00:59 +0300

    lesson complete todos given

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -4,14 +4,29 @@ export class CountDown extends React.Component{
 
     constructor(props){
         super(props)
+        this.state = {count: 100}
     }
+
+    // componentDidMount(){
+    //     this.update()
+    // }
+
+    // update(){
+    //     setInterval(
+    //         () => { 
+    //             let currentCount = this.state.count - 1
+    //             this.setState({count: currentCount})
+    //         }, 1000);
+    // }
+
+    // TODO: WHY DOES IT DECREASE BY 2 INSTEAD OF 1
 
 
     render(){
         return(
             <div>
                 <center>
-                    <h4 className="time-text">100</h4>
+                    <h4 className="time-text">{this.state.count}</h4>
                 </center>
             </div>
         )
